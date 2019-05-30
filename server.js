f55998ae9cf8cef6a1cfb5b10e89bcba9ddce529
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-30 04:01:02 +0100

    set postgres connection details as env variable

--- server.js
+++ server.js
@@ -14,12 +14,15 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'hitesh',
-    password : 'password',
-    database : 'face-recognition'
-  }
+  // connection: {
+  //   host : process.env.POSTGRES_HOST,
+  //   user : process.env.POSTGRES_USER,
+  //   password : process.env.POSTGRES_PASSWORD,
+  //   database : process.env.POSTGRES_DB
+  // }
+
+  // setting from docker-compose.yml
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
